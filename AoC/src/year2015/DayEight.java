cdf288697ffc57c6b82496e22015ddebcc33df56
Solvej Lue <solvejlue@gmail.com>
2023-12-27 13:09:56 +0100

    Day8 done

---
+++ AoC/src/year2015/DayEight.java
@@ -0,0 +1,66 @@
+package year2015;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import utility.Property;
+
+public class DayEight {
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
+		String folder = Property.getFilePathHome();
+		FileReader fr = new FileReader(folder + "/year2015/input/day8.txt");
+		BufferedReader br = new BufferedReader(fr);
+
+		List<String> input = new ArrayList<String>();
+		String t = br.readLine();
+		while(t != null) {
+			input.add(t);
+			t = br.readLine();
+		}
+		br.close();
+
+		int sum = 0;
+		int difference1 = 0; 
+		int difference2 = 0; 
+
+		for (String string : input) {
+			sum += string.length();
+			int count = 0; 
+			int count2 = 0; 
+			for(int i = 0; i < string.length(); i++) {
+				//		System.out.print(string.charAt(i));
+				if(string.charAt(i) == '\\') {
+					if(string.charAt(i + 1) == 'x') {
+						if(string.substring(i + 1, i + 4).matches("[x][0-9a-fA-F]+")) {
+							i += 3;
+							count++; 
+						} 
+					} else {
+						i++; 
+						count++; 
+					}
+				} else {
+					count++;	
+				}
+
+			}
+			for(int i = 0; i < string.length(); i++) {
+				if(string.charAt(i) == '\"' || string.charAt(i) == '\\') {
+					count2++;
+				}
+			}
+			// 2 because of " " in front and back
+			difference1 += count - 2;
+			difference2 += count2 + 2; 
+		}
+		sum = sum - difference1; 
+		System.out.println("Day Eight, Part One: " + sum);
+
+		System.out.println("Day Eight, Part Two: " + difference2);
+	}
+}
