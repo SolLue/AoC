bcd67cf75fe6e1c7f4aece248b3d02929f8c0857
sollu <solvejlue@gmail.com>
2024-12-04 15:36:04 +0100

    Some easy ones done

--- AoC/src/year2017/DayFive.java
+++ AoC/src/year2017/DayFive.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import utility.Property;
 
@@ -14,13 +15,53 @@ public class DayFive {
 		FileReader fr = new FileReader(folder + "/year2017/input/day5.txt");
 		BufferedReader br = new BufferedReader(fr);
 
-		List<String> passphrase = new ArrayList<String>();
+		List<Integer> instructions = new ArrayList<Integer>();
 		String temp = br.readLine();
 		while(temp != null) {
-			passphrase.add(temp);			
+			instructions.add(Integer.parseInt(temp));			
 			temp = br.readLine();
 		}
 		br.close();
-	
+
+		List<Integer> instructionsPart2 = instructions.stream().collect(Collectors.toList());
+		
+		int index = 0; 
+		int steps = 0;
+		boolean outside = false; 
+		while (!outside) {
+			int currentInstruction = instructions.get(index);
+			int jump = currentInstruction + 1;
+			instructions.set(index, jump);
+			index += currentInstruction;
+			steps++;
+			if (index < 0 || index >= instructions.size())
+				outside = true;
+
+		}
+		System.out.println("Day Five, Part One: " + steps);
+
+		index = 0; 
+		steps = 0;
+		outside = false; 
+		while (!outside) {
+			int currentInstruction = instructionsPart2.get(index);
+			
+			int jump = 0; 
+			if(currentInstruction >= 3) {
+				jump = currentInstruction - 1;
+			} else {
+				jump = currentInstruction + 1;
+			}
+			instructionsPart2.set(index, jump);	
+			
+			index += currentInstruction;
+			steps++;
+			if (index < 0 || index >= instructionsPart2.size())
+				outside = true;
+
+		}
+				
+		System.out.println("Day Five, Part One: " + steps);
+
 	}
 }
